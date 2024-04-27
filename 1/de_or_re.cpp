3a4250d084a68d0c96eda425897443c6cc3b62ca
yu666yu666yu666 <1759753124@qq.com>
2024-04-27 22:55:01 +0800

    m

--- 1/de_or_re.cpp
+++ 1/de_or_re.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 
+/*用宏替代new可以更好追踪*/
+
 #define PR_DEBUG 0 //可以随时更改为0或1
 
-#if PR_DEBUG == 1
+#if PR_DEBUG == 1//优于ifdef
 #define LOG(x) std::cout << x << std::endl
 #else
 #define LOG(x)
