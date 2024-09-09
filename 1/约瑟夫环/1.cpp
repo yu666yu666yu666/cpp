f9162df6df3339ebe9e302ebdb62250ec1785b2e
yu666yu666yu666 <1759753124@qq.com>
2024-09-09 21:43:17 +0800

    m

---
+++ 1/约瑟夫环/1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+struct L{
+    int a;
+    int b;
+    L* next;
+};
+
+int main(){
+    int a,b,c;
+    std::cin >> a >> b;
+    c = a;
+    L* head = new L;
+    head->next = nullptr;
+    L* pre = head;
+    while(c){
+        c--;
+        L* p = new L;
+        p->a = (a - c);
+        std::cin >> p->b;
+        if(!c){
+            p->next = head->next;
+            pre->next = p;
+            break;
+        }
+        p->next = nullptr;
+        pre->next = p;
+        pre = pre->next;
+    }
+    pre = head;
+    for(int i = 0;i < a;i++){
+        L* ppre = pre;
+        for(int j = 0;j < b;j++){
+            ppre = pre;
+            pre = pre->next;
+        }
+        ppre->next = pre->next;
+        std::cout << pre->a << ' ';
+        b = pre->b;
+        pre = ppre;
+    }
+}
